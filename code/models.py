b09733597ce8f5dd064bf880ee38f139d155ef8e
shouleizhang <shouleizhangbupt@163.com>
2019-10-09 03:48:06 +0800

    debug

--- code/models.py
+++ code/models.py
@@ -24,7 +24,7 @@ class Encoder(nn.Module):
 		#which should be : (seq_len,batch,embedding_dim)
 		seq_embeded = self.dropout(self.embedding(seq_input)).transpose(0,1)
 		seq_out,h_last = self.encoder_rnn_layer(seq_embeded,hidden_0)
-		return seq_input[:,:,self.encoder_hidden_dim:].transpose(0,1),h_last.transpose(0,1)
+		return seq_out[:,:,self.encoder_hidden_dim:].transpose(0,1),h_last
 	def init_hidden(self):
 		b = self.batch_size
 		num = 2 if self.use_bidirectional else 1
@@ -53,77 +53,82 @@ class Attention(nn.Module):
         self.attn_weight = nn.Linear(input_hidden_dim,output_hidden_dim,bias=False)
         self.softmax = nn.Softmax(dim=-1)
     def forward(self,q,K,V,mask=None):
-        q = q.unsqueeze(1)
+        "notice the query shape is (batch,1,dim)"
         mask = mask.unsqueeze(2)
         K,V = mask * K, mask * V
         projection = self.attn_weight(K)
         attn_socre = torch.bmm(q,projection.transpose(1,2))
         attn_softmax_score = self.softmax(attn_socre)
         combine = torch.bmm(attn_softmax_score,V)
-        return combine.unsqueeze(1)
+        return combine
 
 class AttnEncoderDecoder(nn.Module):
 
-	def __init__(self,Config):
-		super(AttnEncoderDecoder,self).__init__()
-		self.__dict__.update(Config)
-		self.embedding = nn.Embedding(self.output_voc_size, self.embedding_dim)
-		self.dropout = nn.Dropout(self.dropout_prob)
-		self.encoder = Encoder(Config)
-		self.rnn_type_map = {
-			'rnn': nn.RNN,
-			'gru': nn.GRU,
-			'lstm': nn.LSTM,
-		}
-		self.attn = Attention(self.encoder_hidden_dim, self.decoder_hidden_dim)
-		if self.rnn_type not in ['rnn','lstm','gru']:
-			raise NameError('input the appropriate rnn type')
-		self.decoder_rnn_layer = nn.GRU(self.embedding_dim,self.decoder_hidden_dim,
-										self.decoder_rnn_layer_size,bidirectional=True)
-		self.concat_weight = nn.Linear(self.decoder_hidden_dim*2,self.decoder_hidden_dim)
-		self.output_project = nn.Linear(self.decoder_hidden_dim,self.output_voc_size)
-		self.softmax = nn.Softmax(dim=-1)
+    def __init__(self,Config):
+        super(AttnEncoderDecoder,self).__init__()
+        self.__dict__.update(Config)
+        self.embedding = nn.Embedding(self.output_voc_size, self.embedding_dim)
+        self.dropout = nn.Dropout(self.dropout_prob)
+        self.encoder = Encoder(Config)
+        self.rnn_type_map = {
+            'rnn': nn.RNN,
+            'gru': nn.GRU,
+            'lstm': nn.LSTM,
+        }
+        self.attn = Attention(self.encoder_hidden_dim, self.decoder_hidden_dim)
+        if self.rnn_type not in ['rnn','lstm','gru']:
+            raise NameError('input the appropriate rnn type')
+        self.decoder_rnn_layer = nn.GRU(self.embedding_dim,self.decoder_hidden_dim,
+                                        self.decoder_rnn_layer_size,bidirectional=True)
+        self.concat_weight = nn.Linear(self.decoder_hidden_dim*2,self.decoder_hidden_dim)
+        self.output_project = nn.Linear(self.decoder_hidden_dim,self.output_voc_size)
+        self.softmax = nn.Softmax(dim=-1)
 
-	def forward(self,input_token,input_seqs,hidden,mask,first_step=False):
-		#input_token shape : (batch,) after embedding shape : (batch,embed_dim)
-		input_token_embeded = self.dropout(self.embedding(input_token))
-		#add one dimension into input_token and transpose in (0,1) dim
-		#for the format into the rnn input data format
-		input_token_embeded = input_token_embeded.unsqueeze(1).transpose(0,1)
-		#use the t time step hidden as query
-		encoder_seq_hiddens,h = self.encoder(input_seqs)
-		if first_step:
-			decoder_rnn_out,hidden = self.decoder_rnn_layer(input_token_embeded,h)
-		else:
-			decoder_rnn_out,hidden = self.decoder_rnn_layer(input_token_embeded,hidden)
-		#make the shape into (bathc_size,hidden_dim)
-		decoder_rnn_out = decoder_rnn_out[:,:,self.decoder_hidden_dim:].unsqueeze(1)
-		attn_out = self.attn(decoder_rnn_out,encoder_seq_hiddens,encoder_seq_hiddens,mask)
-		concat_fea = torch.concat(attn_out,decoder_rnn_out)
-		out = self.concat_weight(concat_fea)
-		out = self.output_project(out)
-		out = self.softmax(out)
-		return out,hidden
-	def init_hidden(self):
-		b = self.batch_size
-		num = 2 if self.use_bidirectional else 1
-		return torch.zeros(num*self.encoder_rnn_layer_size,b,self.encoder_hidden_dim,device=device)
+    def forward(self,input_token,input_seqs,hidden,mask,first_step=False):
+        #input_token shape : (batch,) after embedding shape : (batch,embed_dim)
+        input_token_embeded = self.dropout(self.embedding(input_token))
+        #add one dimension into input_token and transpose in (0,1) dim
+        #for the format into the rnn input data format
+        input_token_embeded = input_token_embeded.unsqueeze(1).transpose(0,1)
+        #print("input embed shape" ,input_token_embeded.shape)
+        #use the t time step hidden as query
+        encoder_seq_hiddens,h = self.encoder(input_seqs)
+        #print(encoder_seq_hiddens.shape,h.shape)
+        #print('-----------passed-------------')
+        if first_step:
+            decoder_rnn_out,hidden = self.decoder_rnn_layer(input_token_embeded,h)
+        else:
+            decoder_rnn_out,hidden = self.decoder_rnn_layer(input_token_embeded,hidden)
+        #make the shape into (bathc_size,1,hidden_dim)
+        decoder_rnn_out = decoder_rnn_out[:,:,self.decoder_hidden_dim:].transpose(0,1)
+        #print("decoder rnn out",decoder_rnn_out.shape)
+        attn_out = self.attn(decoder_rnn_out,encoder_seq_hiddens,encoder_seq_hiddens,mask)
+        #print(attn_out.shape,decoder_rnn_out.shape)
+        concat_fea = torch.cat((attn_out,decoder_rnn_out),dim=-1).squeeze()
+        out = self.concat_weight(concat_fea)
+        out = self.output_project(out)
+        out = self.softmax(out)
+        return out,hidden
+    def init_hidden(self):
+        b = self.batch_size
+        num = 2 if self.use_bidirectional else 1
+        return torch.zeros(num*self.encoder_rnn_layer_size,b,self.encoder_hidden_dim,device=device)
 
-	def init_weights(self):
-		#simple init method just init all parameters
-		#by the uniform way
-		init_method_map = {
-		'uniform' : partial(init.uniform_,a = self.uniform_lower_bound, b = self.uniform_upper_bound),
-		'normal' : partial(init.normal_, mean = self.normal_mean, std = self.normal_std),
-		'xavier_normal' : partial(init.xavier_normal_, gain = self.xavier_normal_gain),
-		'xavier_uniform' : partial(init.uniform_, gain = self.xavier_uniform_gain),
-		}
-		init_fn = init_method_map[self.init_method]
-		for p in self.parameters():
-			if p.data.dim() == 1 and 'normal' in self.init_method:
-				init.uniform_(p.data)
-			else:
-				init_fn(p.data)
+    def init_weights(self):
+        #simple init method just init all parameters
+        #by the uniform way
+        init_method_map = {
+        'uniform' : partial(init.uniform_,a = self.uniform_lower_bound, b = self.uniform_upper_bound),
+        'normal' : partial(init.normal_, mean = self.normal_mean, std = self.normal_std),
+        'xavier_normal' : partial(init.xavier_normal_, gain = self.xavier_normal_gain),
+        'xavier_uniform' : partial(init.uniform_, gain = self.xavier_uniform_gain),
+        }
+        init_fn = init_method_map[self.init_method]
+        for p in self.parameters():
+            if p.data.dim() == 1 and 'normal' in self.init_method:
+                init.uniform_(p.data)
+            else:
+                init_fn(p.data)
 
 class RNNLMModel(nn.Module):
 	'''
@@ -150,27 +155,25 @@ class RNNLMModel(nn.Module):
 	def get_score(self,x):
 		pass
 def main():
-	model = AttnEncoderDecoder(Config)
-	from data_util import load_data_into_parallel,PaddedTensorLanguageDataset
-	src_file = '../data/nucle/nucle-train.tok.src'
-	trg_file = '../data/nucle/nucle-train.tok.trg'
-	src_trg_pair_langs = load_data_into_parallel(src_file, trg_file)
-	train_dataset = PaddedTensorLanguageDataset(src_trg_pair_langs)
-	from torch.utils.data import RandomSampler
-	from torch.utils.data import DataLoader
-	sampler = RandomSampler(train_dataset)
-	train_dataloader = DataLoader(train_dataset,sampler=sampler,batch_size=16)
-	reversed_train_dataset = PaddedTensorLanguageDataset(src_trg_pair_langs,reversed=True)
-	reversed_train_dataloader = DataLoader(reversed_train_dataset,sampler=sampler,batch_size=16)
-	model.init_weights()
-	for batch in train_dataloader:
-		input_seq,output_seq,input_mask,output_mask = batch
-		hidden = model.init_hidden()
-		print(hidden.shape)
-		print(output_seq[0].dtype)
-		ouput,hidden = model(output_seq[0],input_seq,hidden,input_mask,first_step=True)
-		for i in range(1,Config['max_len']-1):
-			output,hidden = model(output_seq[i],input_seq,hidden,input_mask)
-			print(output.shape)
+    model = AttnEncoderDecoder(Config)
+    from data_util import load_data_into_parallel,PaddedTensorLanguageDataset
+    src_file = '../data/nucle/nucle-train.tok.src'
+    trg_file = '../data/nucle/nucle-train.tok.trg'
+    src_trg_pair_langs = load_data_into_parallel(src_file, trg_file)
+    train_dataset = PaddedTensorLanguageDataset(src_trg_pair_langs)
+    from torch.utils.data import RandomSampler
+    from torch.utils.data import DataLoader
+    sampler = RandomSampler(train_dataset)
+    train_dataloader = DataLoader(train_dataset,sampler=sampler,batch_size=16)
+    reversed_train_dataset = PaddedTensorLanguageDataset(src_trg_pair_langs,reversed=True)
+    reversed_train_dataloader = DataLoader(reversed_train_dataset,sampler=sampler,batch_size=16)
+    model.init_weights()
+    for batch in train_dataloader:
+        input_seq,output_seq,input_mask,output_mask = batch
+        hidden = model.init_hidden()
+        ouput,hidden = model(output_seq[:,0],input_seq,hidden,input_mask,first_step=True)
+        for i in range(1,Config['max_len']-1):
+            output,hidden = model(output_seq[:,i],input_seq,hidden,input_mask)
+            print(output.shape)
 if __name__ == "__main__":
     main()
